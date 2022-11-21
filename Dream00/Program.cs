d5f7fce6fba8aea15d5be69a1fcb5a439d7ada8a
Dmitriy Markulich <mr.kastiiil@mail.ru>
2022-11-21 08:26:06 +0300

    Задача 2

--- Dream00/Program.cs
+++ Dream00/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Напишите программу,которая на 
+//1-вход принимает число и 2 
+// выдает его квадрат
+//Например  
+//4 - 16
+//3 - 9
+//7 - 49
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+// string str = Console.ReadLine();
+// int num Convert.ToInt32(str);
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
---
+++ Dream00/Tusk02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
